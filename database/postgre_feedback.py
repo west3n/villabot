bed234787382637e843f1a49f777b17b196a9b45
Caramba2517 <116070055+Caramba2517@users.noreply.github.com>
2023-03-22 12:29:12 +0400

    Find aps

--- database/postgre_feedback.py
+++ database/postgre_feedback.py
@@ -17,11 +17,25 @@ async def get_update_history(tg_id):
     return history
 
 
+async def get_history(user_id):
+    cur.execute("SELECT history FROM appart_feedback WHERE user_id=%s", (user_id,))
+    result = cur.fetchone()
+    return result
+
+
 async def update_feedback(tg_id, history, data):
     user_id = await status(tg_id)
+    old_history = await get_history(user_id)
+    print(old_history)
     history_str = ''
-    history_str += f'[{datetime.datetime.now()}] User: {history[0]}\n' \
-                   f'[{datetime.datetime.now()}] Admin: {history[1]}'
+    if old_history is None:
+        history_str += f'[{datetime.datetime.now()}] User: {history[0]}\n' \
+                       f'[{datetime.datetime.now()}] Admin: {history[1]}'
+    if old_history is not None:
+        history_str += f'{old_history[0]}\n'
+        history_str += f'[{datetime.datetime.now()}] User: {history[0]}\n' \
+                       f'[{datetime.datetime.now()}] Admin: {history[1]}'
+
     cur.execute("UPDATE appart_feedback SET text=%s, answer=NULL, history=%s WHERE user_id=%s",
                 (data.get('text'), history_str, user_id,))
     db.commit()
